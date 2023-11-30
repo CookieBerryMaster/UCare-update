0207189d5b13a8b1c75af297640d9a4d2241267b
sofiia-0 <ana.sofiia02@gmail.com>
2023-11-29 21:13:04 -0600

    Añadidura de comentarios explicativos a clase utilitaria Global.

--- src/main/java/org/example/UCare/util/Global.java
+++ src/main/java/org/example/UCare/util/Global.java
@@ -1,6 +1,9 @@
+// Paquete que contiene la clase Global
 package org.example.UCare.util;
 
+// Clase utilitaria para almacenar valores globales accesibles desde diferentes partes del programa
 public class Global {
 
+    // Campo estático que almacena el nombre de usuario, es accesible desde cualquier parte del código
     public static String userName = null;
 }
