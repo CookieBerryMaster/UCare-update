c44daefc5564b9a0913beb897066c4068a3e82de
cherry <cvmedina@uamv.edu.ni>
2023-11-29 18:26:27 -0600

    Actualizacion

--- src/main/java/org/example/UCare/model/EstadoDeAnimo.java
+++ src/main/java/org/example/UCare/model/EstadoDeAnimo.java
@@ -1,6 +1,14 @@
 package org.example.UCare.model;
 
-public class EstadoDeAnimo {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Entity
+public class EstadoDeAnimo extends Ids {
     public enum ESTADO{Feliz,Bien,Cansado,Estresado,Triste};
     private ESTADO estado;
     private String comentario;
