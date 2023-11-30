f1269fa93936de7f7eaa0ae2e0838f0333855f71
sofiia-0 <ana.sofiia02@gmail.com>
2023-11-29 21:55:02 -0600

    Añadidura de comentarios explicativos a clase utilitaria CalcularEstudiante.

--- src/main/java/org/example/UCare/calculators/CalcularEstudiante.java
+++ src/main/java/org/example/UCare/calculators/CalcularEstudiante.java
@@ -1,17 +1,23 @@
+// Paquete que contiene la clase CalcularEstudiante
 package org.example.UCare.calculators;
 
+// Importaciones necesarias
 import org.example.UCare.model.Estudiantes;
 import org.example.UCare.util.Global;
 import org.openxava.calculators.ICalculator;
 import static org.openxava.jpa.XPersistence.getManager;
 
-
+// Clase que implementa la interfaz ICalculator de OpenXava para calcular el objeto Estudiantes
 public class CalcularEstudiante implements ICalculator {
+
+    // Método calculate() implementado de la interfaz ICalculator
     @Override
     public Object calculate() throws Exception {
-        final Estudiantes estudiante = getManager() // getManager() de XPersistence
-                .find(Estudiantes.class, Global.userName); // Busca el producto
+        // Obtener el EntityManager utilizando XPersistence
+        final Estudiantes estudiante = getManager()
+                // Buscar un objeto Estudiantes por su identificador (nombre de usuario)
+                .find(Estudiantes.class, Global.userName);
+        // Devolver el objeto Estudiantes encontrado
         return estudiante;
-
     }
 }
