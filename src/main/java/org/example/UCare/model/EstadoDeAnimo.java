1ad66427a4b53e116311199fe4deca855f692756
sofiia-0 <ana.sofiia02@gmail.com>
2023-11-29 23:54:30 -0600

    Añadidura de comentarios explicativos a clase EstadoDeAnimo.

--- src/main/java/org/example/UCare/model/EstadoDeAnimo.java
+++ src/main/java/org/example/UCare/model/EstadoDeAnimo.java
@@ -1,7 +1,11 @@
+// Paquete y nombre de la clase para la organización del código
 package org.example.UCare.model;
 
+// Importación de las anotaciones Lombok para generar automáticamente getters y setters
 import lombok.Getter;
 import lombok.Setter;
+
+// Importación de clases y utilidades necesarias
 import org.example.UCare.calculators.CalcularEstudiante;
 import org.example.UCare.calculators.FechaCalculator;
 import org.example.UCare.filter.FilterEstudiante;
@@ -15,41 +19,49 @@ import javax.validation.constraints.AssertTrue;
 import java.util.Calendar;
 import java.util.Date;
 
+// Anotaciones para la generación automática de getters y setters, y marcando la clase como una entidad JPA
 @Getter
 @Setter
 @Entity
-@View(members = "estadoDeAnimo[" + " estado;" + " comentario;" + " fecha;" + " ];" + "Ucare{estudiantes}")
-@Tab(properties = "estado,estudiantes.nombre, comentario",
-        baseCondition = "${estudiantes.cif}=?", filter= FilterEstudiante.class)
-public class EstadoDeAnimo extends Ids {
-    public enum ESTADO{Feliz, Bien, Cansado, Estresado, Triste};
 
+// Anotación para definir la disposición de los campos en la vista y las pestañas
+@View(members = "estadoDeAnimo[" + " estado;" + " comentario;" + " fecha;" + " ];" + "Ucare{estudiantes}")
+
+// Anotación para definir propiedades de la tabla, como las columnas que se mostrarán y la condición base
+@Tab(properties = "estado,estudiantes.nombre, comentario", baseCondition = "${estudiantes.cif}=?", filter= FilterEstudiante.class)
+public class EstadoDeAnimo extends Ids {
+    // Enumeración que representa los posibles estados de ánimo
+    public enum ESTADO {Feliz, Bien, Cansado, Estresado, Triste};
+
+    // Campo que representa el estado de ánimo, marcado como obligatorio
     @Required
     private ESTADO estado;
 
-    // Utiliza la anotación @Stereotype para convertir el campo en una caja grande de texto
+    // Campo que representa el comentario, con anotaciones para configurar su presentación
     @Stereotype("TEXT_AREA")
-
-    // Utiliza la anotación @DisplaySize para especificar el número de caracteres que se mostrarán en la lista
     @DisplaySize(10)
     @Column(length = 240)
     private String comentario;
 
+    // Campo que representa la fecha, con un calculador automático para la fecha actual y marcado como de solo lectura
     @DefaultValueCalculator(CurrentDateCalculator.class)
     @ReadOnly
     private Date fecha;
 
+    // Relación muchos a uno con la entidad Estudiantes, marcado como obligatorio con un calculador automático
     @ManyToOne
     @Required
     @DefaultValueCalculator(value= CalcularEstudiante.class)
     private Estudiantes estudiantes;
 
+    // Método oculto para validar que el comentario no exceda los 240 caracteres
     @Hidden
     @AssertTrue(message = "El campo solo acepta 240 caracteres!")
     public boolean iscomentariovalido() {
         return comentario != null && comentario.length() < 239;
     }
 
+    // Método oculto para validar que el estado de ánimo se registre después de las 8 PM y antes de las 12 AM
     @Hidden
     @AssertTrue(message = "Solo se puede registrar un estado de ánimo después de las 8 PM y antes de las 12 AM")
     public boolean isHoraValida() {
