53ce82e049ff6a55c187df1764be2a4163799846
sofiia-0 <ana.sofiia02@gmail.com>
2023-11-29 23:39:34 -0600

    Añadidura de comentarios explicativos a clase Recordatorios.

--- src/main/java/org/example/UCare/model/Recordatorios.java
+++ src/main/java/org/example/UCare/model/Recordatorios.java
@@ -13,27 +13,35 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+// La anotación @Tab especifica cómo se debe mostrar y filtrar la entidad en la interfaz de usuario.
 @Tab(
         properties = "fechaHora, realizado, actividad.nombreDeActividad",
         baseCondition = "${actividad.estudiantes.cif}=?",
         filter = FilterEstudiante.class
 )
+// La anotación @View define la apariencia de la vista de la entidad en la interfaz de usuario.
 @View(
         members = "nuevoRecordatorio[fechaHora, realizado, actividad];")
 public class Recordatorios extends Ids {
 
+    // La anotación @Required indica que el campo no puede ser nulo.
     @Required
+    // La anotación @Stereotype especifica el tipo de dato y la presentación del campo en la interfaz de usuario.
     @Stereotype("DATETIME_SQL")
     private Timestamp fechaHora;
 
     private boolean realizado;
 
+    // La anotación @ManyToOne indica una relación muchos a uno con la entidad Actividades.
     @ManyToOne
     @Required
     private Actividades actividad;
 
+    // La anotación @Hidden oculta el campo en la interfaz de usuario.
     @Hidden
+    // La anotación @AssertTrue realiza una validación personalizada.
     @AssertTrue(message = "La fecha y hora deben ser mayores o iguales a la actual")
+    // Este método verifica si la fecha y hora son mayores o iguales a la fecha y hora actuales.
     public boolean isFechaHoraMayorOIgual() {
         return fechaHora != null && (fechaHora.toLocalDateTime().isAfter(LocalDateTime.now()) || fechaHora.toLocalDateTime().isEqual(LocalDateTime.now()));
     }
