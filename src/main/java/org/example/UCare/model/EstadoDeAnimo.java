7e06b5faf02d356d7cc74d2ccc32097c6172cdfe
cherry <cvmedina@uamv.edu.ni>
2023-11-29 18:24:18 -0600

    Actualizacion

--- src/main/java/org/example/UCare/model/EstadoDeAnimo.java
+++ src/main/java/org/example/UCare/model/EstadoDeAnimo.java
@@ -1,4 +1,7 @@
 package org.example.UCare.model;
 
 public class EstadoDeAnimo {
+    public enum ESTADO{Feliz,Bien,Cansado,Estresado,Triste};
+    private ESTADO estado;
+    private String comentario;
 }
